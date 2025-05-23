fd4e65260edd936bc34d90fc8596621c76d7d43e
Zach Daniel <zach@zachdaniel.dev>
2025-05-22 22:42:15 -0400

    chore: cleanup unneeded files

--- test/projects/project_with_rules/test/project_with_rules_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ProjectWithRulesTest do
-  use ExUnit.Case
-  doctest ProjectWithRules
-
-  test "greets the world" do
-    assert ProjectWithRules.hello() == :world
-  end
-end
--- test/projects/project_without_rules/test/project_without_rules_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ProjectWithoutRulesTest do
-  use ExUnit.Case
-  doctest ProjectWithoutRules
-
-  test "greets the world" do
-    assert ProjectWithoutRules.hello() == :world
-  end
-end
